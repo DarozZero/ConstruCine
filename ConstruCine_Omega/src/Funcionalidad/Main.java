e2f7efe9cccc0add0b24137f29bbca3e42badf0d
DarozZero <jose.daroz.aviles@gmail.com>
2022-12-06 05:06:19 -0600

    cambio en main

--- ConstruCine_Omega/src/Funcionalidad/Main.java
+++ ConstruCine_Omega/src/Funcionalidad/Main.java
@@ -1,15 +1,20 @@
 package Funcionalidad;
 
-import Interfaces.Cartelera;
 import ModeloLogin.Sistema;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Main {
 
     
-    public static void main(String[] args) throws IOException {
-     Sistema sistema = new Sistema();
+    public static void main(String[] args){
+        try {
+            Sistema sistema = new Sistema();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
